29794fbeb6ecb9a661d4faf42de3c9f16cbb676c
godzillalogan <kenlogan0117@gmail.com>
2021-07-30 17:21:31 +0800

    post方法那邊 有調整

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+mongoose.connect('mongodb://localhost/shorten-URL', {
+  useNewUrlParser:true, useUnifiedTopology:true
+})
 
 const routes = require('./routes')  // 引用路由器
 // require('./config/mongoose')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)  // 將 request 導入路由器
 app.use(express.static('public'))  //靜態檔案
 // setting template engine, extname: '.hbs'，是指定副檔名為.hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,21 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+const ShortUrl = require('../../models/shortUrl')
 
-router.get('/', (req, res) => {
-  res.render('index')
+
+
+router.get('/',async (req , res) =>{
+  const shortUrls = await ShortUrl.find().lean()
+  res.render('index', { shortUrls: shortUrls})
 })
 
+router.post('/shortUrls',(req , res) => {
+  const fullURL = req.body.fullURL
+  console.log(fullURL)
+  return ShortUrl.create({ full:fullURL })     // 存入資料庫
+  .then(() => res.redirect('/')) // 新增完成後導回首頁
+  .catch(error => console.log(error))
+})
 
 module.exports = router
